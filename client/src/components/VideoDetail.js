a9aab013adb397e839fbde1ed4315893c5126ede
smessiah777 <smessiah17@gmail.com>
2019-08-06 17:21:08 -0500

    video list and video detail components layout adjusted

--- client/src/components/VideoDetail.js
+++ client/src/components/VideoDetail.js
@@ -5,9 +5,14 @@ import React from 'react';
 const VideoDetail = ({video}) => {
     if (!video){
         return <div>Loading...</div>;
-    }
+    } 
+
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
     return (
         <div>
+            <div className="ui embed">
+                <iframe src={videoSrc}></iframe>
+            </div>
             <div className="ui segment">
                 <h4 className="ui header">
                 {video.snippet.title}
